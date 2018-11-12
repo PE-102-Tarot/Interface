440e348d40d86eda22499483747d4f3c9102829a
LeopoldDelay <43851684+LeopoldDelay@users.noreply.github.com>
2018-11-12 11:25:46 +0100

    Update Human.py
    Add exchange w/ chien when petite

--- Human.py
+++ Human.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Oct 29 14:31:52 2018
 
-@author: PE_102
+@author: TeamIATECH
 """
 from Player import Player
 from Trump import Trump
@@ -13,6 +13,7 @@ from Excuse import Excuse
 class Human(Player):
     def __init__(self, hand, score, name):
         Player.__init__(self, hand, score, name)
+        self.__hand= hand
     
     def play(self, trick):
         if trick==[]:
@@ -38,15 +39,81 @@ class Human(Player):
         self.get_hand().remove(playable_cards[card])
         return (playable_cards[card])
     
-    def bid(self):
+    def bid(self,dog):
         listeBid = ["Passe","Petite","Garde","Garde sans","Garde contre"] 
         while True:
             print(listeBid)
-            print("\nVos cartes")
+            print("\nVoici vous cartes : ")
             print(self.get_hand())
-            choice = input("Votre choix de Prise? (Ecrire en lettre)")
+            choice = input("\nVotre choix de Prise? (Ecrire en lettre)")
             for i in range(len(listeBid)):
                 if choice == listeBid[i]:
+                    if choice == "Garde": #Le joueuur à choisi Garde
+                        still_choosing =True#Il lui est donc proposé d'échanger ses cartes avec le chien(dog)
+                        print("\nVous avez choisi Garde")
+                        print("\nVous Pouvez échanger vos cartes avec celles qui sont dans le chien")
+                        print("\Tapez 69 lorsque vous avez terminé")
+                        while still_choosing:  
+                             player_cards=Player.get_hand(self)
+                             print("\nVos cartes : \n")
+                             for i, el in enumerate(player_cards):
+                                 print(str(i)+"-->"+str(el))
+                             print("\nLes cartes du chien")
+                             for i,el in enumerate(dog):
+                                 print(str(i)+"-->"+str(el))
+                             n1 = int(input("Le numero de la carte à mettre dans le chien"))
+                             n2 = int(input("Le numero de la carte à mettre dans votre jeu"))
+                             
+                             print("\n-----------------------------------------------------------\n")
+                             if n1 == 69 or n2 == 69:
+                                 still_choosing = False
+                                 break
+                             elif n1 < 19 and n2<6 : 
+                                 hand = self.get_hand()
+                                 
+                                 card1 = hand.pop(n1)
+                                 card2 = dog.pop(n2)
+                                 dog.append(card1)
+                                 hand.append(card2)
+                                 self.set_hand(hand)
+                             else:
+                                 print("Veuillez renseigner un numero valide")
+                    if choice == "Petite": #Le joueuur à choisi Garde
+                        still_choosing =True#Il lui est donc proposé d'échanger ses cartes avec le chien(dog)
+                        print("\nVous avez choisi Petite")#bite
+                        print("\nVous Pouvez échanger vos cartes avec celles qui sont dans le chien")
+                        print("\Tapez 69 lorsque vous avez terminé")
+                        while still_choosing:  
+                             player_cards=Player.get_hand(self)
+                             print("\nVos cartes : \n")
+                             for i, el in enumerate(player_cards):
+                                 print(str(i)+"-->"+str(el))
+                             print("\nLes cartes du chien")
+                             for i,el in enumerate(dog):
+                                 print(str(i)+"-->"+str(el))
+                             n1 = int(input("Le numero de la carte à mettre dans le chien"))
+                             n2 = int(input("Le numero de la carte à mettre dans votre jeu"))
+                             
+                             print("\n-----------------------------------------------------------\n")
+                             if n1 == 69 or n2 == 69:
+                                 still_choosing = False
+                                 break
+                             elif n1 < 19 and n2<6 : 
+                                 hand = self.get_hand()
+                                 
+                                 card1 = hand.pop(n1)
+                                 card2 = dog.pop(n2)
+                                 dog.append(card1)
+                                 hand.append(card2)
+                                 self.set_hand(hand)
+                             else:
+                                 print("Veuillez renseigner un numero valide")
+                                 
+                            
+                            
+                        
+                        
+                
                     return choice
             print("\nArgument non valide")
 
